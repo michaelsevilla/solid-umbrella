7deb42b110984e31e8cc8c2af9a2d4363d48a58a
Michael A. Sevilla <mikesevilla3@gmail.com>
2026-09-25 11:02:27 -0700

    update model
    Signed-off-by: Michael A. Sevilla <mikesevilla3@gmail.com>

--- test/pr_reviewer.py
+++ test/pr_reviewer.py
@@ -49,7 +49,7 @@ def main():
     # 3. Analyze the diff with Gemini
     client = genai.Client(api_key=gemini_key)
     
-    chat = client.chats.create(model='gemini-1.5-flash')
+    chat = client.chats.create(model='gemini-1.5-flash-latest')
     response = chat.send_message(
         prompt,
         config=types.GenerateContentConfig(response_mime_type="application/json")
